b6e044b07aa2e2df8c9f7134c08634fc49f67d75
risahruth <187304967+risahruth@users.noreply.github.com>
2026-05-25 10:45:18 +0000

    Initial prisma connection to the db

--- memoapp/src/app/page.tsx
+++ memoapp/src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+    const users = await prisma.user.findMany();
+
     return (
         <main>
-            <h1>Hello World</h1>
+            <h1>MemoryMap</h1>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
         </main>
     );
 }
